f13c34cda849275f6f4a00e1057c25ee09470051
Mary Kate F. Anecito <marykate09@protonmail.com>
2024-03-12 16:21:11 +0800

    Updated Anime and Genre Models to handle timestamps in pivot table and also seeded the table_anime_genres (refer to DatabaseSeeder.php)

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -10,4 +10,17 @@ class Anime extends Model
     use HasFactory;
 
     protected $table = 'table_animes';
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'table_anime_genres', 'anime_id', 'genre_id')->withTimestamps();
+        /* belongsToMany means that the relationship is many-to-many.
+         * The first argument is the name of the related model.
+         * The second argument is the name of the pivot table (the table that is used to store the relationships).
+         * The third argument is the foreign key of the model
+         * on which you are defining the relationship, while
+         * the fourth argument is the foreign key of the model.
+         */
+    }
+
 }
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -10,4 +10,19 @@ class Genre extends Model
     use HasFactory;
 
     protected $table = 'table_genres';
+    protected $fillable = ['genre'];
+    // This fillable property is used to allow mass assignment of the genre column. In layman's term it allows you to insert data into the genre column.
+
+    public function animes()
+    {
+        return $this->belongsToMany(Anime::class, 'table_anime_genres', 'genre_id', 'anime_id')->withTimestamps();
+        /* belongsToMany means that the relationship is many-to-many.
+         * The first argument is the name of the related model.
+         * The second argument is the name of the pivot table (the table that is used to store the relationships).
+         * The third argument is the foreign key of the model
+         * on which you are defining the relationship, while
+         * the fourth argument is the foreign key of the model
+         * that you are joining to.
+         */
+    }
 }
