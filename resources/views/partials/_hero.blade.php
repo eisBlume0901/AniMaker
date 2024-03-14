b2e9766282142c9ac14ae9143326245b91f35066
Mary Kate Anecito <marykate09@protonmail.com>
2024-03-14 22:36:11 +0800

    Changed the hover effect of the navigation links by adding a background color instead of giving them a font-weight

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -7,7 +7,7 @@
     <title>AniMaKer</title>
     <link
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous"
         referrerpolicy="no-referrer"
@@ -20,7 +20,10 @@
         <a href="#" class="hover:text-blue-700"><img src="{{asset('images/AniMaKer_Official_Logo.png')}}" alt="AniMaKer Official Logo" class="w-1/6"></a>
     </div>
     <div class="mr-16">
-        <a href="#"><i class=""></i></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 text-white hover:text-blue-700">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+        </a>
     </div>
 </div>
 <nav class="bg-blue-700 text-white py-2">
@@ -41,11 +44,11 @@
 {{--                    </div>--}}
 {{--                </div>--}}
 
-                <button class="genre-button md:ml-16 md:mt-0 hover:font-bold">GENRE</button>
+                <button class="genre-button md:ml-16 md:mt-0  hover:bg-white hover:text-blue-700 rounded-2xl px-3 py-0.5">Genre</button>
                 <div id="genre-dropdown" class="absolute w-80 mt-5 lg:ml-16 md:mx-auto rounded-lg bg-white shadow-lg shadow-emerald-300 ring-1 ring-black ring-opacity-5 focus:outline-none hidden" tabindex="-1">
                     <div class="flex flex-wrap p-2">
                         @foreach($genres as $genreName)
-                            <div class="lg:w-1/2 sm:w-full md:w-1/2 text-blue-700 text-sm px-4 py-2 hover:bg-emerald-400 hover:text-white hover:rounded-lg hover:font-bold active:bg-blue-700">
+                            <div class="lg:w-1/2 sm:w-full md:w-1/2 text-blue-700 text-sm px-4 py-2 hover:bg-emerald-400 hover:text-white hover:rounded-3xl hover:font-bold active:bg-blue-700">
                                 <a href="/?genre={{$genreName->genre}}" id="{{$genreName->id}}-genre">{{ $genreName->genre}}
                             </div>
                         @endforeach
@@ -87,18 +90,21 @@
             <script>
                 const genreButton = document.querySelector('.genre-button');
 
-                genreButton.addEventListener('click', function() {
-                    document.getElementById('genre-dropdown').classList.toggle('hidden');
+                genreButton.addEventListener('mouseover', function() {
+                    document.getElementById('genre-dropdown').classList.add('hidden');
                 });
 
+                genreButton.addEventListener('mouseout', function() {
+                    document.getElementById('genre-dropdown').classList.remove('hidden');
+                });
             </script>
 
             <a href="#"></a>
         </div>
         <ul class="flex flex-col md:flex-row items-center">
-            <li class="md:mr-16 mt-3 md:mt-0"><a href="#" class="hover:font-bold">My List</a></li>
-            <li class="md:mr-16 mt-3 md:mt-0"><a href="#" class="hover:font-bold">My Favorites</a></li>
-            <li class="md:mr-16 mt-3 md:mt-0"><a href="#" class="hover:font-bold">Top Rated</a></li>
+            <li class="md:mr-16 mt-3 md:mt-0"><a href="#" class="hover:bg-white hover:text-blue-700 rounded-2xl px-3 py-0.5">My List</a></li>
+            <li class="md:mr-16 mt-3 md:mt-0"><a href="#" class="hover:bg-white hover:text-blue-700 rounded-2xl px-3 py-0.5">My Favorites</a></li>
+            <li class="md:mr-16 mt-3 md:mt-0"><a href="#" class="hover:bg-white hover:text-blue-700 rounded-2xl px-3 py-0.5">Top Rated</a></li>
         </ul>
     </div>
 </nav>
