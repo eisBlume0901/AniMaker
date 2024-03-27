617f064eea6a89ca7851159f9d96999ae34887c3
Mary Kate Anecito <marykate09@protonmail.com>
2024-03-27 17:58:04 +0800

    Implemented dynamic genre selection in anime creation form. Still subjected if it should be fetched from the database or it is hardcoded

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class AnimeController extends Controller
 {
     //
     public function create() {
-        return view('animes/create');
+        return view('animes/create', [
+            'genres' => Genre::all()
+            ]);
     }
 
 
--- resources/views/animes/create.blade.php
+++ resources/views/animes/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <form action="/anime" method="POST"
-          class="m-10 p-10 w-full max-w-lg mx-auto overflow-hidden bg-white rounded-2xl shadow-blue-700 shadow-md hover:shadow-emerald-400 transition ease-in-out duration-150">
+          class="m-10 p-10 w-full max-w-xl mx-auto overflow-hidden bg-white rounded-2xl shadow-blue-700 shadow-md hover:shadow-emerald-400 transition ease-in-out duration-150">
         @csrf
         <div class="flex m-2 flex-row justify-center align-middle">
             <div class="flex m-2 flex-col justify-center align-middle">
@@ -50,97 +50,98 @@
 
             <div id="dropdownSearch" class="z-10 hidden bg-white rounded-2xl shadow shadow-emerald-400 w-52 p-2">
                 <ul class="h-48 overflow-y-auto text-md text-blue-700">
+                    @foreach($genres as $genre)
                     <li>
                         <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="action" name="genre_id" type="checkbox" value="1"
+                            <input id="{{$genre->genre}}" name="genre_id" type="checkbox" value="{{$genre->id}}"
                                    class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="action"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Action</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="adventure" name="genre_id" type="checkbox" value="2"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="adventure" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Adventure</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="avant_garde" name="genre_id" type="checkbox" value="3"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="avant_garde" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Avant
-                                Garde</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="comedy" name="genre_id" type="checkbox" value="4"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="comedy"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Comedy</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="drama" name="genre_id" type="checkbox" value="5"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="drama"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Drama</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="fantasy" name="genre_id" type="checkbox" value="6"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="fantasy"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Fantasy</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="horror" name="genre_id" type="checkbox" value="7"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="horror"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Horror</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="mystery" name="genre_id" type="checkbox" value="8"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="mystery"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Mystery</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="romance" name="genre_id" type="checkbox" value="9"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="romance"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Romance</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="sci_fi" name="genre_id" type="checkbox" value="10"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="sci_fi"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Sci-Fi</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="slice_of_life" name="genre_id" type="checkbox" value="11"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="slice_of_life" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Slice
-                                of Life</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="sports" name="genre_id" type="checkbox" value="12"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="sports"
-                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Sports</label>
-                        </div>
-
-                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">
-                            <input id="supernatural" name="genre_id" type="checkbox" value="13"
-                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">
-                            <label for="supernatural" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Supernatural</label>
+                            <label for="{{$genre->genre}}"
+                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">{{$genre->genre}}</label>
                         </div>
                     </li>
+                    @endforeach
                 </ul>
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="adventure" name="genre_id" type="checkbox" value="2"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="adventure" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Adventure</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="avant_garde" name="genre_id" type="checkbox" value="3"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="avant_garde" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Avant--}}
+{{--                                Garde</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="comedy" name="genre_id" type="checkbox" value="4"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="comedy"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Comedy</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="drama" name="genre_id" type="checkbox" value="5"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="drama"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Drama</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="fantasy" name="genre_id" type="checkbox" value="6"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="fantasy"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Fantasy</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="horror" name="genre_id" type="checkbox" value="7"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="horror"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Horror</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="mystery" name="genre_id" type="checkbox" value="8"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="mystery"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Mystery</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="romance" name="genre_id" type="checkbox" value="9"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="romance"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Romance</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="sci_fi" name="genre_id" type="checkbox" value="10"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="sci_fi"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Sci-Fi</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="slice_of_life" name="genre_id" type="checkbox" value="11"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="slice_of_life" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Slice--}}
+{{--                                of Life</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="sports" name="genre_id" type="checkbox" value="12"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="sports"--}}
+{{--                                   class="w-full ms-2 text-md font-medium text-blue-700 rounded">Sports</label>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-blue-50 active:bg-emerald-50">--}}
+{{--                            <input id="supernatural" name="genre_id" type="checkbox" value="13"--}}
+{{--                                   class="w-4 h-4 text-emerald-400 focus:ring-emerald-400 bg-blue-100 border-blue-100 rounded">--}}
+{{--                            <label for="supernatural" class="w-full ms-2 text-md font-medium text-blue-700 rounded">Supernatural</label>--}}
+{{--                        </div>--}}
             </div>
 
             <div class="my-6">
@@ -154,12 +155,30 @@
                                 d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                         </svg>
                     </div>
-                    <input datepicker datepicker-autohide type="text"
-                           class="ps-10 bg-blue-50 block w-full px-4 py-2 mt-2 border border-blue-100 shadow-md shadow-blue-50 text-blue-700 text-md rounded-2xl focus:ring-emerald-400 focus:border-emerald-400 focus:text-emerald-700 focus:bg-emerald-50"
-                           required>
+                    <input datepicker datepicker-autohide type="text" id="start_aired_date" name="start_aired_date" class="ps-10 bg-blue-50 block w-full px-4 py-2 mt-2 border border-blue-100 shadow-md shadow-blue-50 text-blue-700 text-md rounded-2xl focus:ring-emerald-400 focus:border-emerald-400 focus:text-emerald-700 focus:bg-emerald-50" required/>
                 </div>
             </div>
 
+            <div class="my-6">
+                <label for="end_aired_date" class="block text-blue-700 mb-2 text-md font-semibold">End Aired
+                    Date</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-blue-700 focus:text-emerald-700" xmlns="http://www.w3.org/2000/svg"
+                             fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                    </div>
+                    <input datepicker datepicker-autohide type="text" id="end_aired_date" name="end_aired_date" class="ps-10 bg-blue-50 block w-full px-4 py-2 mt-2 border border-blue-100 shadow-md shadow-blue-50 text-blue-700 text-md rounded-2xl focus:ring-emerald-400 focus:border-emerald-400 focus:text-emerald-700 focus:bg-emerald-50"/>
+                </div>
+            </div>
+
+            <div class="my-6 flex min-w-full justify-center">
+                <button class="px-4 py-2 text-base font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-700 rounded-2xl hover:bg-emerald-400">
+                    Add Anime
+                </button>
+            </div>
         </div>
     </form>
 @endsection
