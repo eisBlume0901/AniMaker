3e16fad7aab9aef287725ef27ea60285dc59f833
Marvin Local Flores <flores2220255@mkt.ceu.edu.ph>
2024-04-02 18:18:08 +0800

    updated anime-card.blade.php, tentative on description because too long

--- resources/views/components/anime-card.blade.php
+++ resources/views/components/anime-card.blade.php
@@ -1,19 +1,51 @@
-
-    <div class="card mt-8">
+@props(['specificAnime'])
+<div class="max-w-sm m-10 bg-white rounded-2xl">
+    <a href="#">
+        <img class="rounded-t-lg block w-auto h-3/4 object-cover rounded-2xl shadow-md shadow-blue-700 hover:opacity-75 hover:shadow-emerald-400 transition ease-in-out duration-150"
+             src="{{$specificAnime->image ? asset('storage/' .$specificAnime->image) : asset('images/no-image-1.png')}}"
+             alt="{{$specificAnime->title}} image">
+    </a>
+    <div class="p-5">
         <a href="#">
-            <img src="https://m.media-amazon.com/images/W/MEDIAX_849526-T1/images/I/81+V2uHQT1L._AC_SX679_.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
-        </a>
-        <div class="mt-2 font-bold text-blue-700">
-            <a href="#">Frieren: Beyond Journey's End</a>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-blue-700">{{$specificAnime->title}}</h5>
+        </a> <div class="flex items-center space-x-1 rtl:space-x-reverse">
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
         </div>
-        <div class="stats flex items-center text-blue-700 text-sm mt-1">
-            <span class="">Score:</span>
-            <span class="ml-1">9.13</span>
-            <span class="mx-2">|</span>
-            <span>Sept 29, 2023</span>
+        <div class="rounded-md mt-3 bg-white min-w-40 text-blue-700 overflow-auto z-10">
+            <div class="w-50 h-3 stats flex flex-row items-center text-blue-700 mt-1 content-center">
+                <span class="">Score:</span>
+                <span class="ml-1">9.13</span>
+                <span class="mx-1">|</span>
+                @php
+                    $date = date_create_from_format('Y-m-d', $specificAnime->start_aired_date);
+                    $formattedDate = $date->format('M d, Y');
+                @endphp
+                <span>{{$formattedDate}}</span>
+            </div>
+            <div class="w-50 content-center mt-1 {{ count($specificAnime->genres) > 3 ? 'h-10' : 'h-5' }}">
+                @foreach($specificAnime->genres as $genre)
+                    <span class="mt-0">{{$genre->genre}}</span>
+                    @if(!$loop->last)
+                        <span class="mx-1">|</span>
+                    @endif
+                @endforeach
+            </div>
+            <p class="mt-3 mb-3 font-normal text-gray-700">{{$specificAnime->reviews}}</p>
         </div>
-        <div class="genre text-blue-700 text-sm">
-            Adventure, Drama, Fantasy
-        </div>
-        <button class="rounded bg-blue-700 text-white px-3 py-1.5 mt-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-blue-400 active:bg-emerald-400">Add to List</button>
     </div>
+</div>
+
