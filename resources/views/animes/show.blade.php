04e1407b4b24e6ca13e1d69774d83a069c0cff98
Myrandde <115961838+Myrandde@users.noreply.github.com>
2024-04-02 22:18:21 +0800

    Update show.blade.php
    Updated show.blade.php based on frieren.html file and master's show.blade.php

--- resources/views/animes/show.blade.php
+++ resources/views/animes/show.blade.php
@@ -1,27 +1,48 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="anime-info border-b border-blue-700">
-        <div class="mx-auto px-16 py-16 flex">
-            <img src="https://m.media-amazon.com/images/W/MEDIAX_849526-T1/images/I/81+V2uHQT1L._AC_SX679_.jpg" alt="" class="w-80">
-            <div class="ml-20">
-                <h2 class="text-2xl font-semibold">Frieren: Beyond's Journey's End</h2>
-                <div class="stats flex items-center text-blue-700 text-sm">
-                    <span class="">Score:</span>
-                    <span class="ml-1">9.13</span>
-                    <span class="mx-2">|</span>
-                    <span>Sept 29, 2023</span>
-                    <span class="mx-2">|</span>
-                    <span>Adventure, Drama, Fantasy</span>
+    <div class="mx-auto px-16 py-16 flex max-w-full">
+                <img src="{{$anime->image ? asset('storage/' .$anime->image) : asset('images/no-image-1.png')}}" alt="{{$anime->title}} image" alt="{{$anime->title}}" class="w-80">
+                <div class="ml-20 flex flex-col">
+                    <div class = "flex flex-row align-middle justify-between">
+                        <div>
+                            <h2 class="text-4xl font-semibold">{{$anime->title}}</h2>
+                            <div class="stats flex items-center text-blue-700 text-sm">
+                            </div>
+                            <div class="flex-row mt-4">
+                                @foreach($anime->genres as $genre)
+                                    <a href="" class="text-blue-700 m-3">{{$genre->genre}}</a>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div>
+                            <div class="flex-row m-10">
+                                <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-3xl shadow-emerald-400 shadow-md hover:opacity-75 hover:shadow-emerald-400 transition ease-in-out duration-150">Add to List</a>
+                                <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-3xl shadow-emerald-400 shadow-md hover:opacity-75 hover:shadow-emerald-400 transition ease-in-out duration-150">Update</a>
+                                <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-3xl shadow-emerald-400 shadow-md hover:opacity-75 hover:shadow-emerald-400 transition ease-in-out duration-150">Delete</a>
+                            </div>
+                        </div>
+                    </div>  
+                    <div class = "flex flex-col justify-between">
+                            <div class = "flex flex-row text-center text-xl"> Air Span: 
+                                @php
+                                    $startdate = date_create_from_format('Y-m-d', $anime->start_aired_date);
+                                    $formattedDate = $startdate->format('M d, Y');
+                                @endphp
+                                {{$$formattedstartDate}} - 
+                                @php
+                                    $enddate = date_create_from_format('Y-m-d', $anime->end_aired_date);
+                                    $formattedendDate = $startdate->format('M d, Y');
+                                @endphp
+                                {{$$formattedendDate}} </span> </div> 
+                            <div class = "flex flex-row text-center text-xl"> Episodes: {{$anime->episodes}} </span> </div> 
+                            <div class = "flex flex-row text-center text-xl"> Studio: {{$anime->studio}} </span> </div>
+                        </div>
+                    <p class="text-blue-700 mt-8 text-xl">
+                    {{$anime->description}}
+                    </p>
                 </div>
-                <p class="text-blue-700 mt-8">
-                    During their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles, creating unforgettable precious memories for most of them.
-                    However, the time that Frieren spends with her comrades is equivalent to merely a fraction of her life, which has lasted over a thousand years. When the party disbands after their victory, Frieren casually returns to her "usual" routine of collecting spells across the continent. Due to her different sense of time, she seemingly holds no strong feelings toward the experiences she went through.
-                    As the years pass, Frieren gradually realizes how her days in the hero's party truly impacted her. Witnessing the deaths of two of her former companions, Frieren begins to regret having taken their presence for granted; she vows to better understand humans and create real personal connections. Although the story of that once memorable journey has long ended, a new tale is about to begin.
-                </p>
             </div>
 
-        </div>
 
-    </div>
 @endsection
