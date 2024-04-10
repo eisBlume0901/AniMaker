b8c3e6a7798d87f47f8f93e2a71faaf9a2475edd
Mary Kate Anecito <marykate09@protonmail.com>
2024-04-10 12:45:46 +0800

    Updated navigation bar for user/guest

--- resources/views/partials/animeNavBar.blade.php
+++ resources/views/partials/animeNavBar.blade.php
@@ -1,24 +1,18 @@
-<nav class="bg-blue-700 text-white py-2">
-    <div class="mx-auto flex flex-col md:flex-row items-center justify-between px-3 py-1">
-        <div class="flex flex-col md:flex-row items-center">
-            <div class="relative">
-                <label>
-                    <input type="text" class="bg-white text-blue-700  rounded-full w-50 px-2 py-1 focus:outline-none focus:shadow-outline md:ml-16" placeholder=" Search">
-                </label>
-            </div>
-            <div class="relative">
-                <button id="genre-button" class="hover:bg-white hover:text-blue-700 hover:font-bold rounded-2xl px-3 py-1 md:ml-16">Genre</button>
-                <div id="genre-dropdown" class="absolute mt-5 z-10 w-80 lg:ml-16 md:mx-auto rounded-lg bg-white shadow-lg shadow-blue-700 hover:shadow-emerald-400 ring-1 ring-black ring-opacity-5 focus:outline-none hidden p-2" tabindex="-1">
-                    <div class="flex flex-wrap" id="genre-entry">
-                        @foreach($genres as $genreName)
-                            <div class="w-1/2 text-blue-700 text-sm px-4 py-2 bg-white rounded-2xl hover:bg-blue-700 hover:text-white hover:rounded-3xl hover:font-bold active:bg-gradient-to-br active:from-emerald-400 active:to-blue-700 transition ease-in-out duration-150">
-                                <a href="/?genre={{$genreName->genre}}" id="{{$genreName->id}}-genre">{{ $genreName->genre}}
-                            </div>
-                        @endforeach
-                    </div>
+<nav class="bg-blue-700 text-white">
+    <div class="max-w-screen-3xl flex flex-wrap items-center justify-between mx-auto px-20 py-4">
+        <div class="flex flex-row gap-x-20">
+            <label>
+                <input type="text" class="bg-white text-blue-700 rounded-full w-50 px-2 py-1 focus:outline-none focus:shadow-outline" placeholder=" Search">
+            </label>
+            <button id="genre-button" class="hover:text-emerald-400 hover:border-b-emerald-400 px-2 -ml-8">Genre</button>
+            <div id="genre-dropdown" class="absolute mt-10 ml-60 z-10 w-80 rounded-lg bg-white shadow-lg shadow-blue-700 hover:shadow-emerald-400 ring-1 ring-black ring-opacity-5 focus:outline-none hidden p-2" tabindex="-1">
+                <div class="flex flex-wrap" id="genre-entry">
+                    @foreach($genres as $genreName)
+                        <div class="w-1/2 text-blue-700 text-sm px-4 py-2 bg-white rounded-2xl hover:bg-blue-700 hover:text-white hover:rounded-3xl hover:font-bold active:bg-gradient-to-br active:from-emerald-400 active:to-blue-700 transition ease-in-out duration-150">
+                            <a href="/?genre={{$genreName->genre}}" id="{{$genreName->id}}-genre">{{ $genreName->genre}}
+                        </div>
+                    @endforeach
                 </div>
-
-
             </div>
             <script>
                 const genreButton = document.getElementById('genre-button');
@@ -34,13 +28,11 @@
                 });
             </script>
 
-            <a href="#"></a>
         </div>
-        <div class="relative">
-            <ul class="flex flex-col md:flex-row items-center">
-                <li class="md:mr-14 mt-3 md:mt-0"><a href="#" class="hover:bg-white hover:text-blue-700 hover:font-bold rounded-2xl px-3 py-1 ">My List</a></li>
-                <li class="md:mr-14 mt-3 md:mt-0"><a href="#" class="hover:bg-white hover:text-blue-700 hover:font-bold rounded-2xl px-3 py-1">Top Rated</a></li>
-            </ul>
+        <div class="flex flex-row gap-x-20">
+{{--            Still have to put the route and its controller--}}
+            <a href="/animes/toprated" class="hover:text-emerald-400">Top Rated</a>
+            <a href="/users/mylist" class="hover:text-emerald-400">My List</a>
         </div>
 
     </div>
