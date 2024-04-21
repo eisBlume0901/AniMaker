9645383a85dbfe35f70cac506a413b93d1cd4693
Mary Kate Anecito <marykate09@protonmail.com>
2024-04-21 17:05:12 +0800

    Updated edit-user template

--- resources/views/admin/edit-user.blade.php
+++ resources/views/admin/edit-user.blade.php
@@ -4,22 +4,22 @@
 @section('content')
     @include('partials.adminManageAnimeNavBar')
     <form action="/user/{{$user->id}}" method="POST" enctype="multipart/form-data"
-          class="m-5 p-5 w-full max-w-3xl mx-auto overflow-hidden bg-white rounded-2xl shadow-blue-700 shadow-md hover:shadow-emerald-400 transition ease-in-out duration-150">
+          class="mt-10 w-full max-w-screen-md mx-auto overflow-hidden bg-white rounded-2xl shadow-blue-700 shadow-md hover:shadow-emerald-400 transition ease-in-out duration-150">
 
         @csrf
 
         @method('PUT')
 
-        <div class="flex m-2 flex-row justify-center align-middle">
+        <div class="flex m-2 flex-row justify-center align-middle w-full">
             <div class="flex m-2 flex-col justify-center align-middle">
                 <h1 class="font-semibold">Edit user details</h1>
             </div>
         </div>
 
-        <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-2">
+        <div class="flex flex-row sm:flex-col md:flex-row lg:flex-row gap-x-6 items-center justify-center content-center w-full p-5">
             <div class="mb-6">
                 <div class="flex flex-col items-center mt-2">
-                    <div class="w-60 h-72 mb-2 overflow-hidden rounded-2xl shadow-md shadow-blue-700 hover:shadow-emerald-400">
+                    <div class="w-48 h-48 rounded-full mb-2 overflow-hidden shadow-md shadow-blue-700 hover:shadow-emerald-400">
                         <img id='preview_img' class="w-full h-full object-cover"
                              src="{{$user->image ? asset('storage/' .$user->image) : asset('images/no-image-1.png')}}"
                              alt="Current profile photo" />
@@ -58,7 +58,7 @@
 
 
 
-            <div class="grid-cols-3">
+            <div class="flex flex-col flex-wrap w-full">
 
                 <div class="mb-6">
                     <label for="name" class="block text-blue-700 text-md font-semibold mb-2 ml-2 my-0.5">Name</label>
@@ -83,7 +83,33 @@
                     @enderror
                 </div>
 
+                <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6">
+{{--                    Change the data matching the data--}}
+                    <div class="mb-6">
+                        <label for="number_of_animes" class="block text-blue-700 text-md font-semibold mb-2 ml-2">Number of Animes</label>
+                        <input type="number" name="number_of_animes" id="email"
+                               class="w-full px-3 py-2 text-blue-700 border border-blue-100 bg-blue-50 shadow-md shadow-blue-50 text-md rounded-2xl focus:ring-emerald-400 focus:border-emerald-400 focus:text-emerald-700 focus:bg-emerald-50"
+                               value="{{$user->id}}"
+                               placeholder="24" >
 
+                        @error('id')
+                        <p class="text-red-500 text-sm mx-3 my-2">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+{{--                    Change the data matching the data--}}
+                    <div class="mb-6">
+                        <label for="number_of_reviews" class="block text-blue-700 text-md font-semibold mb-2 ml-2">Number of Reviews</label>
+                        <input type="number" name="number_of_reviews" id="number_of_reviews"
+                               class="w-full px-3 py-2 text-blue-700 border border-blue-100 bg-blue-50 shadow-md shadow-blue-50 text-md rounded-2xl focus:ring-emerald-400 focus:border-emerald-400 focus:text-emerald-700 focus:bg-emerald-50"
+                               value="{{$user->id}}"
+                               placeholder="24" >
+
+                        @error('id')
+                        <p class="text-red-500 text-sm mx-3 my-2">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -92,7 +118,7 @@
 
 
 
-        <div class="mt-6 flex min-w-full justify-center">
+        <div class="mb-6 flex min-w-full justify-center">
             <button class="px-4 py-2 text-base font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-700 rounded-2xl hover:bg-emerald-400">
                 Update User
             </button>
